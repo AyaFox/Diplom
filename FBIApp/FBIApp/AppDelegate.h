cf01156f5a69a16a3ebd184b2c6d6b5d4806912a
AyaFox <pritty.yana@me.com>
2014-10-06 22:09:38 -0700

    Сделала каркас приложения

---
+++ FBIApp/FBIApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FBIApp
+//
+//  Created by Яна on 06.10.14.
+//  Copyright (c) 2014 Yana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
